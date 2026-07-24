8dc418c90b486dbec96229bdcd60f226b0a59119
Om Sanjaysingh Bais <omthakur0718@gmail.com>
2026-07-24 12:40:47 +0530

    added cleaning function

--- scripts/search_demo.py
+++ scripts/search_demo.py
@@ -1,15 +1,17 @@
 from app.utils.classifier import llm_planner
 from app.utils.search_executor import search
+from app.utils.cleaner import clean
 
 query = "what is agentic ai?"
 plan = llm_planner(query)
 # print(plan)
 
 response = search(query, max_result=5 if plan["research_require"] == "true" else 3)
-for item in response:
-    print(item["raw_content"])
 
-# print("-"*100)
-# result = search("what is who?")
-# for item in result:
-#     print(item['url'])
+raw_texts = []
+for item in response:
+    raw_texts.append(item["raw_content"])
+
+records = clean(raw_texts)
+for record in records:
+    print(record["clean"])

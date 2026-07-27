cdac13af1d447ae69d8bdf2a269068608632c345
Om Sanjaysingh Bais <omthakur0718@gmail.com>
2026-07-27 13:05:05 +0530

    feat: added retrieval chain

--- app/rag/chain.py
+++ app/rag/chain.py
@@ -2,31 +2,27 @@
 """
 A function that returns complete Retrieval chain, contains retrival and augmentation bundled in single function.
 input: None
-output: RAG chain
+output: Retrieval chain
 """
 
 from langchain_classic.chains import create_retrieval_chain
 from langchain_classic.chains.combine_documents import (
     create_stuff_documents_chain,
 )
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 from langchain_groq import ChatGroq
 from app.rag.ingestor import get_vectorstore
 
-PROMPT = PromptTemplate(
-    input_variables=["context", "input"],
-    template="""
-    You are a research assistant, use only provided context to answer, always site source URLs at the end of your answer.
-    If the context dosen't contain the enough information, say so explicitly.
+system_prompt = """You are a research assistant, use only provided context to answer, always site source URLs at the end of your answer.
+    If the context doesn't contain the enough information, say so explicitly.
+    Context: {context}"""
 
-    context: {context}
-    question: {input}
-    Answer(with sources):
-    """,
+PROMPT = ChatPromptTemplate.from_messages(
+    [("system", system_prompt), ("human", "{input}")]
 )
 
 
-def get_rag_chain():
+def build_rag_chain():
     """function that loads persisted vector store from the disk and return Retrieval chain built over it."""
     vectorstore = get_vectorstore()  # loads from disk
     retriever = vectorstore.as_retriever(search_kwargs={"k": 5})

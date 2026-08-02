3d6ddc8f0b33dd6063bd297b5fa557437dc47bae
Om Sanjaysingh Bais <omthakur0718@gmail.com>
2026-08-02 18:06:01 +0530

    agent now using custom tool for searcing

--- app/agent/search_agent.py
+++ app/agent/search_agent.py
@@ -1,16 +1,17 @@
 # app/agent/research_agent.py
 
 """
-this script builds LangGraph's ReAct agent that uses Tavily web searching tool to search user query over internet, returns its result.
+this script builds LangGraph's ReAct agent that uses custom Tavily web searching tool to search user query over internet, returns its result.
 """
 
 from langchain.agents import create_agent
 from langchain_groq import ChatGroq
-from langchain_tavily import TavilySearch
 from langgraph.checkpoint.memory import MemorySaver
-from config import GROQ_API_KEY
 
-MODEL = "llama-3.3-70b-versatile"
+from config import GROQ_API_KEY
+from app.agent.search_tool import make_search_tool
+
+MODEL = "qwen/qwen3.6-27b"
 
 memory = MemorySaver()  # to provide memory to an agent, one shared store
 
@@ -23,7 +24,7 @@ def build_agent():
         model=MODEL,
         temperature=0,
     )
-    tool = TavilySearch(max_result=2, include_raw_content="text")
+    tool, _ = make_search_tool()
     return create_agent(model=llm_model, tools=[tool], checkpointer=memory)
 
 
@@ -31,11 +32,13 @@ def run_agent(query: str, session_id: str) -> dict:
     agent = build_agent()
     config = {"configurable": {"thread_id": session_id}}
     return agent.invoke(
-        {"messages": [{"role": "user", "content": query}]}, config=config
-    )  # type: ignore
+        {"messages": [{"role": "user", "content": query}]},
+        config=config,  # type: ignore
+    )
 
 
 if __name__ == "__main__":
-    query = "what was the reason behind chernobyl disaster?"
+    query = "How Quantum Computing can be used to cure Cancer?"
+
     result = run_agent(query, "12jk3")
     print(result)

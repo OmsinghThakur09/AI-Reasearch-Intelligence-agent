30b6ac1fb0d109a78c34eeb0c4ba342b177b1155
Om Sanjaysingh Bais <omthakur0718@gmail.com>
2026-08-02 18:05:03 +0530

    custom search tool to store raw_content

--- app/agent/search_tool.py
+++ app/agent/search_tool.py
@@ -5,18 +5,16 @@ custom search tool for searching web and only sending 'url', 'title', 'content' 
 
 from langchain_core.tools import tool
 from langchain_community.utilities.tavily_search import TavilySearchAPIWrapper
-from app.utils.cleaner import clean
 from config import TAVILY_API_KEY
-from app.db.queries import save_documents, save_sources
-import uuid
 
 _tavily = TavilySearchAPIWrapper(tavily_api_key=TAVILY_API_KEY)  # type: ignore
 
 
-def make_search_tool(query_id: uuid.UUID):
+def make_search_tool():
     """
     Factory function to create custom search tool for each user query
     """
+    raw_content = []
 
     @tool("web_search_tool")
     def web_search_tool(search_query: str):
@@ -26,37 +24,38 @@ def make_search_tool(query_id: uuid.UUID):
         "and send all info except raw_content to ReAct agent"
 
         raw_results = _tavily.raw_results(
-            query=search_query, max_results=2, include_raw_content=True
+            query=search_query,
+            max_results=2,
+            include_raw_content="text",  # type: ignore
         )
 
         results = raw_results.get("results", [])
         if not results:
             return "result not found"
 
-        raw_content = [r.get("raw_content", "") for r in results]
+        raw_content.append(r.get("raw_content", "") for r in results)
 
-        raw_clean_dict = clean(raw_content)
+        # raw_clean_dict = clean(raw_content)
 
         metadatas = []
-        for result in results:
-            for item in result:
-                metadatas.append(
-                    {
-                        "url": item["url"],
-                        "title": item["title"],
-                        "content": item.get("content", ""),
-                    }
-                )
+        for item in results:
+            metadatas.append(
+                {
+                    "url": item["url"],
+                    "title": item["title"],
+                    "content": item.get("content", ""),
+                }
+            )
 
-        save_documents(query_id, metadatas, raw_clean_dict)
+        # save_documents(query_id, metadatas, raw_clean_dict)
 
-        save_sources(query_id, metadatas)
+        # save_sources(query_id, metadatas)
 
-        return (
-            f"Found {len(metadatas)} result(s), full content stored in database for further process:\n\n"
-            + f"{metadatas}"
-            + "\n\nUse these summaries to judge whether you have enough information "
-            "to answer, or need to search again with a more specific query."
-        )
+        return {
+            "instruction": f"Found {len(metadatas)} result(s), full content stored in database for further process:\n"
+            + "\nUse below summaries to judge whether you have enough information "
+            "to answer, or need to search again with a more specific query.",
+            "metadata": metadatas,
+        }
 
-    return web_search_tool
+    return web_search_tool, raw_content
